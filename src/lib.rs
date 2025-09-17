646e73d0040abe6aceeba3f3cc6f60ed598022f0
Placeless <thinkoverheat@gmail.com>
2025-09-18 03:15:58 +0900

    No cutting esp

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,11 @@ mod iso;
 mod utils;
 
 /// High-level function to create the FAT32 image and then the final ISO.
-pub fn create_disk_and_iso(iso_path: &Path, efi_path: &Path) -> io::Result<()> {
-    // create_fat32_image(fat32_img_path, bellows_path, kernel_path)?;
-    create_iso_from_img(iso_path, efi_path)?;
+pub fn create_disk_and_iso(iso_path: &Path, bellows_path: &Path, kernel_path: &Path) -> io::Result<()> {
+    let fat32_img_path = Path::new("./fat32.img");
+
+    create_fat32_image(fat32_img_path, bellows_path, kernel_path)?;
+
+    create_iso_from_img(iso_path, fat32_img_path)?;
     Ok(())
 }
