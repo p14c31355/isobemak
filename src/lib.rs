d4b3ff3a207402d414e00c2821e6051c3dd9b873
Placeless <thinkoverheat@gmail.com>
2025-09-20 07:49:39 +0900

    Fix Nsect

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // isobemak/src/lib.rs
 use crate::fat::create_fat_image;
 use crate::iso::create_iso_from_img;
-use std::{io, io::Write, path::Path};
+use std::{io, io::Write, path::Path, fs};
 use tempfile::NamedTempFile;
 
 mod fat;
@@ -20,17 +20,15 @@ pub fn create_disk_and_iso(
     let mut fat_img_file = NamedTempFile::new()?;
     let fat_img_path = fat_img_file.path().to_owned();
 
-    // 2. Create and populate the FAT32 filesystem.
-    // Capture the actual size returned by create_fat32_image.
-    let fat_image_actual_size =
+    let _fat_image_actual_size =
         create_fat_image(fat_img_file.as_file_mut(), loader_path, kernel_path)?;
 
-    // Ensure the file is flushed to disk before create_iso_from_img reads it.
+
     fat_img_file.as_file_mut().flush()?;
 
-    // 3. Create the ISO, embedding the temporary FAT32 filesystem.
-    // Pass the actual size to create_iso_from_img.
-    create_iso_from_img(iso_path, &fat_img_path, fat_image_actual_size)?;
+    let loader_metadata = fs::metadata(loader_path)?;
+    let loader_size = loader_metadata.len() as u32;
+    create_iso_from_img(iso_path, &fat_img_path, loader_size)?;
 
     // 4. The temporary file will be automatically deleted when it goes out of scope.
     println!("create_disk_and_iso: Process complete. ISO created successfully.");
