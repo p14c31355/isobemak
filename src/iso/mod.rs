59f96f29b9629c5862044ec4a4dc6609b1edff36
Placeless <thinkoverheat@gmail.com>
2025-09-21 06:19:43 +0900

    Refactor iso.rs

--- src/iso/mod.rs
+++ src/iso/mod.rs
@@ -3,6 +3,3 @@ pub mod boot_catalog;
 pub mod dir_record;
 pub mod iso;
 pub mod volume_descriptor;
-
-// `create_iso_from_img` is no longer re-exported here.
-// It will be accessed directly via `iso::iso::create_iso_from_img`.
